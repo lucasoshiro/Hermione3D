6bf62eea8e8b6f92daba85f262c8a65c08c4cbb4
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2024-07-29 19:39:12 -0300

    increase max z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1498,7 +1498,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS (Y_MIN_POS + Y_BED_SIZE)
-#define Z_MAX_POS 140
+#define Z_MAX_POS 170
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
